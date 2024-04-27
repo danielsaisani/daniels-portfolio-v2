05381b2cd814b4d5730e58605e2c133f9aed6034
Daniel Saisani <danielsaisani@gmail.com>
2024-04-27 15:50:12 +0100

    fix darn thang

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -4,13 +4,13 @@ import Skills from "../components/Skills";
 export default function Page() {
   return (
   <>
-    <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
-    </section>
-      <section>
-          <Projects/>
-          <Skills/>
-      </section>
+<section>
+  <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+</section>
+  <section>
+      <Projects/>
+      <Skills/>
+  </section>
   </>
   );
 }
