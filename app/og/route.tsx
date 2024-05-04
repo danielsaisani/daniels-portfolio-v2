ceed5b0c790b288fd703532e3fc5bcede5b66472
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-05-04 21:40:43 +0100

    Update route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -3,13 +3,13 @@ import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = req.nextUrl;
-  const postTitle = searchParams.get('title');
-  const font = fetch(
-    new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
-  ).then((res) => res.arrayBuffer());
-  const fontData = await font;
-
-  return null
-}
+// export async function GET(req: NextRequest) {
+//   const { searchParams } = req.nextUrl;
+//   const postTitle = searchParams.get('title');
+//   const font = fetch(
+//     new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
+//   ).then((res) => res.arrayBuffer());
+//   const fontData = await font;
+//
+//   return null
+// }
