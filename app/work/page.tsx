b933275f616ece44d5cc3d95ed715e7e23f63630
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-05-04 18:02:11 +0100

    Update page.tsx

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,10 +1,14 @@
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+
 export default function Page() {
   return (
-    <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
-      </p>
-    </section>
+      <section>
+          <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
+          <section>
+              <Projects/>
+              <Skills/>
+          </section>
+      </section>
   );
 }
