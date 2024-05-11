26e769b2f78ab0d4a964d94cbf64dc9aac8679f7
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-05-11 10:45:52 +0100

    fix

--- app/db/postgres.ts
+++ app/db/postgres.ts
@@ -3,5 +3,5 @@ import postgres from 'postgres';
 const testing = process.env.TESTING==="true"
 
 export const sql = postgres(process.env.POSTGRES_URL!, {
-  ssl: testing!,
+  ssl: !testing,
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 export default function Page() {
   return (
+      <>
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">hey there, i'm Daniel 👾</h1>
       <p className="prose prose-neutral dark:prose-invert">
          Feel free to chill out and take a look around my page and see what i've been up to and what i've been thinking about!
       </p>
     </section>
-  );
+    <section>
+    </section>
+  </>
+          );
 }
