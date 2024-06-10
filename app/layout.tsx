3a4e7998fa69e13f1e328568d62216f16f882e34
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-06-10 23:14:37 +0100

    add posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,14 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SandpackCSS } from './blog/[slug]/sandpack';
 import { NextUIProvider } from "@nextui-org/react";
+import posthog from 'posthog-js'
+
+posthog.init('phc_dh52uHLuDUsmafEF0ONkFb6EEDkw12n7Fx6fUCRTRAe',
+    {
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.BASE_URL!),
