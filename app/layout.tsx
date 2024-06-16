b29b7cbd491abeb8cfb5b821fd2da8760b9f1a7b
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-06-16 12:39:22 +0100

    recordings

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,12 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SandpackCSS } from './blog/[slug]/sandpack';
 import { CSPostHogProvider } from './providers'
+import posthog from 'posthog-js'
+
+if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
+    posthog.init('phc_dh52uHLuDUsmafEF0ONkFb6EEDkw12n7Fx6fUCRTRAe', { api_host: 'https://eu.i.posthog.com', person_profiles: 'identified_only' })
+    posthog.startSessionRecording()
+}
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.BASE_URL!),
@@ -61,18 +67,18 @@ export default function RootLayout({
       )}
     >
     <CSPostHogProvider>
-    <head>
-      <link rel="icon" href="/static/favicon.ico" sizes="any"/>
-      <SandpackCSS/>
-    </head>
-    <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+      <head>
+        <link rel="icon" href="/static/favicon.ico" sizes="any"/>
+        <SandpackCSS/>
+      </head>
+      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
       <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-         {/*<NextUIProvider>*/}
+        {/*<NextUIProvider>*/}
         <Navbar/>
         {children}
         <Analytics/>
         <SpeedInsights/>
-         {/*</NextUIProvider>*/}
+        {/*</NextUIProvider>*/}
       </main>
       </body>
     </CSPostHogProvider>
