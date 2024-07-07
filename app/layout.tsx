40222ed56f1e0481b8c4628fe2158f36626d32c8
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-07-07 14:39:28 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
+"use client";
+
 import './global.css';
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-import { Poppins } from 'next/font/google'
+import { Poppins } from 'next/font/google';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SandpackCSS } from './blog/[slug]/sandpack';
-import { CSPostHogProvider } from './providers'
+import { CSPostHogProvider } from './providers';
 import BottomBar from "./components/BottomBar";
 import { usePathname } from 'next/navigation';
 
@@ -49,14 +51,13 @@ export const metadata: Metadata = {
 
 const cx = (...classes) => classes.filter(Boolean).join(' ');
 
-const poppins = Poppins({ weight:"500", subsets: ['latin'] })
+const poppins = Poppins({ weight:"500", subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
   const pathname = usePathname();
   const isBlogPage = pathname.startsWith('/blog');
 
@@ -68,25 +69,25 @@ export default function RootLayout({
         poppins.className,
       )}
     >
-    <CSPostHogProvider>
-      <head>
-        <link rel="icon" href="/img/favicon.ico" sizes="any"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png"/>
-        <link rel="manifest" href="/img/site.webmanifest"/>
-        <SandpackCSS/>
-      </head>
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-      <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-        <Navbar/>
-        {children}
-        <Analytics/>
-        <SpeedInsights/>
-        {!isBlogPage && <BottomBar />}
-      </main>
-      </body>
-    </CSPostHogProvider>
+      <CSPostHogProvider>
+        <head>
+          <link rel="icon" href="/img/favicon.ico" sizes="any"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png"/>
+          <link rel="manifest" href="/img/site.webmanifest"/>
+          <SandpackCSS/>
+        </head>
+        <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            <Navbar/>
+            {children}
+            <Analytics/>
+            <SpeedInsights/>
+            {!isBlogPage && <BottomBar />}
+          </main>
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
