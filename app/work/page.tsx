2fa783ccf7eed0a5df18cfc2009fe38577836671
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-07-07 12:52:22 +0100

    refactor projects and add links

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 import {Card, Image} from "@nextui-org/react";
 import React from "react";
-import {CardHeader} from "@nextui-org/card";
+import Link from "next/link";
+import Project from "../components/Project";
 
 export default function Page() {
 
@@ -10,51 +11,34 @@ export default function Page() {
           <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
           <div className="gap-4 grid grid-cols-12 py-8">
 
-              <Card className="col-span-4 h-[300px] hover:scale-[1.05] duration-200" isHoverable>
-                  <Image
-                      removeWrapper
-                      alt="Fiverr logo"
-                      className="z-0 w-full h-full object-cover rounded-3xl"
-                      src="/static/project-2.gif"
-                  />
-                  <CardHeader content={'hello'}/>
-              </Card>
+              <Project projectPicture={'project-2.gif'} projectUrl={'https://github.com/DxnielKS/pacman-mdp'}/>
 
-              <Card className="col-span-4 h-[300px] hover:scale-[1.05] duration-200" isHoverable>
-                  <Image
-                      removeWrapper
-                      alt="Fiverr logo"
-                      className="z-0 w-full h-full object-cover rounded-3xl"
-                      src="/static/project-5.png"
-                  />
-              </Card>
+              <Project projectPicture={'project-5.png'} projectUrl={'https://turingmachines.netlify.app/'}/>
 
-              <Card className="col-span-4 h-[300px] hover:scale-[1.05] duration-200">
-                  <Image
-                      removeWrapper
-                      alt="Fiverr logo"
-                      className="z-0 w-full h-full object-cover rounded-3xl"
-                      src="/static/tourbi.png"
-                  />
-              </Card>
+              <Project projectPicture={'tourbi.png'} projectUrl={'https://github.com/DxnielKS/tour-bi'}/>
 
-              <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5 hover:scale-[1.05] duration-200">
-                  <Image
-                      removeWrapper
-                      alt="Fiverr logo"
-                      className="z-0 w-full h-full object-cover rounded-3xl"
-                      src="/static/tiktok.png"
-                  />
-              </Card>
 
-              <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7 hover:scale-[1.05] duration-200">
-                  <Image
-                      removeWrapper
-                      alt="Fiverr logo"
-                      className="z-0 w-full h-full object-cover rounded-3xl"
-                      src="/static/path-finder.gif"
-                  />
-              </Card>
+                <Link href={'https://www.tiktok.com/@clipscartel'} target={'_blank'}>
+                  <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5 hover:scale-[1.05] duration-200">
+                      <Image
+                          removeWrapper
+                          alt="tiktok incubator"
+                          className="z-0 w-full h-full object-cover rounded-3xl"
+                          src="/static/tiktok.png"
+                      />
+                  </Card>
+                </Link>
+
+                <Link href={'https://github.com/DxnielKS/path-finder'} target={'_blank'}>
+                  <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7 hover:scale-[1.05] duration-200">
+                      <Image
+                          removeWrapper
+                          alt="path finder"
+                          className="z-0 w-full h-full object-cover rounded-3xl"
+                          src="/static/path-finder.gif"
+                      />
+                  </Card>
+               </Link>
           </div>
       </section>
 );
