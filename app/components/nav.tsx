593b7d90e91269252ed268150ffc6eb39b85c290
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-10-05 22:53:01 +0100

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -41,28 +41,41 @@ export function Navbar() {
   return (
     <nav className={`tracking-tight -ml-[8px] ease-in-out transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
       <div className='flex w-full justify-start cursor-pointer md:hidden'>
-        <Image 
-          onClick={() => setMobileOpen(!mobileOpen)} 
-          className='text-2xl' 
-          width={30} 
-          height={30} 
-          src={mobileOpen ? '/pause-outline.svg' : '/menu-outline.svg'} 
-          alt='menu' 
+        <Image
+          onClick={() => setMobileOpen(!mobileOpen)}
+          className={`text-2xl ${mobileOpen ? 'rotate-90' : ''} duration-300`}
+          width={30}
+          height={30}
+          src={'/menu-outline.svg'}
+          alt='menu'
         />
       </div>
-      <div className={`md:flex ${mobileOpen ? 'flex' : 'hidden'} flex-col md:flex-row justify-start items-center gap-4 md:gap-10 md:space-x-0 pr-10`}>
-        {Object.entries(navItems).map(([path, { name }]) => {
-          return (
-            <Link
-              key={path}
-              href={path}
-              className={`${path === currentPage ? 'font-extrabold' : 'font-light'} duration-300 transition-all hover:-translate-y-1 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2`}
-              onClick={() => setMobileOpen(false)}
-            >
-              {name}
-            </Link>
-          );
-        })}
+      <div 
+        className={`
+          overflow-hidden transition-all duration-300 ease-in-out
+          md:h-auto md:opacity-100
+          ${mobileOpen ? '' : 'h-0'}
+        `}
+      >
+        <div className={`
+          flex flex-col md:flex-row justify-start items-center gap-4 md:gap-10 md:space-x-0 pr-10
+          transition-transform duration-300 ease-in-out
+          ${mobileOpen ? 'translate-y-0' : '-translate-y-full'}
+          md:translate-y-0
+        `}>
+          {Object.entries(navItems).map(([path, { name }]) => {
+            return (
+              <Link
+                key={path}
+                href={path}
+                className={`${path === currentPage ? 'font-extrabold' : 'font-light'} duration-300 transition-all hover:-translate-y-1 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2`}
+                onClick={() => setMobileOpen(false)}
+              >
+                {name}
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </nav>
   );
