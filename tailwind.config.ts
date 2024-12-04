cd89747b9e583e9e81e32583efd742dd2e9fc987
DKS <danielsaisani@gmail.com>
2024-12-04 16:45:40 +0000

    [Style] add shadcn styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,49 +3,65 @@ import typography from '@tailwindcss/typography';
 
 export default {
   content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx', './public/**/*.svg'],
-  darkMode: "class",
+  darkMode: ["class", 'class'],
   theme: {
-    extend: {
-      colors: {
-        dark: "#191516",
-        light: "#E6E8E6",
-        primary: "#512888",
-        secondary: "#062A77",
-        tertiary: "#B11225"
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        typewriter: {
-          from: { width: '0' },
-          to: { width: '24em' }
-        },
-        caret: {
-          '50%': { borderColor: 'transparent' },
-        },
-      },
-      animation: {
-        fadeIn: 'fadeIn 1s ease-in-out',
-        // caret: 'caret 2s steps(1) infinite',
-      },
-      fontFamily: {
-        sans: ['var(--font-geist-sans)'],
-        mono: ['var(--font-geist-mono)'],
-        'poppins': ['Poppins']
-
-      },
-      
-      typography: {
-        quoteless: {
-          css: {
-            'blockquote p:first-of-type::before': { content: 'none' },
-            'blockquote p:first-of-type::after': { content: 'none' },
-          },
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			dark: '#191516',
+  			light: '#E6E8E6',
+  			primary: '#512888',
+  			secondary: '#062A77',
+  			tertiary: '#B11225'
+  		},
+  		keyframes: {
+  			fadeIn: {
+  				'0%': {
+  					opacity: '0'
+  				},
+  				'100%': {
+  					opacity: '1'
+  				}
+  			},
+  			typewriter: {
+  				from: {
+  					width: '0'
+  				},
+  				to: {
+  					width: '24em'
+  				}
+  			},
+  			caret: {
+  				'50%': {
+  					borderColor: 'transparent'
+  				}
+  			}
+  		},
+  		animation: {
+  			fadeIn: 'fadeIn 1s ease-in-out'
+  		},
+  		fontFamily: {
+  			sans: ['var(--font-geist-sans)'],
+  			mono: ['var(--font-geist-mono)'],
+  			poppins: ['Poppins']
+  		},
+  		typography: {
+  			quoteless: {
+  				css: {
+  					'blockquote p:first-of-type::before': {
+  						content: 'none'
+  					},
+  					'blockquote p:first-of-type::after': {
+  						content: 'none'
+  					}
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   future: {
     hoverOnlyWhenSupported: true,
@@ -60,6 +76,7 @@ export default {
         },
       }
       addUtilities(newUtilities, ['responsive', 'hover'])
-    }
-  ],
+    },
+      require("tailwindcss-animate")
+],
 } satisfies Config;
