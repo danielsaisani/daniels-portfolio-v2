bcc7b2ed3c43c050f68bf72be19c12cf3664c2a2
DKS <47062269+DxnielKS@users.noreply.github.com>
2025-04-13 10:39:15 +0100

    Add ability to request one or all blogs

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,33 +1,51 @@
 import { NextRequest } from 'next/server'
 import { z } from 'zod'
 
-const BlogServerApiResponseSchema = z.object({
+const BlogServerApiBlogsResponseSchema = z.object({
     data: z.array(
         z.object({
             id: z.number(),
             documentId: z.string(),
             title: z.string(),
             description: z.string().nullable(),
-            slug: z.string(),
+            slug: z.string().nullable(),
             publishedAt: z.string(),
             updatedAt: z.string(),
         })
     ).nullable()
 })
 
+const BlogServerApiBlogResponseSchema = z.object({
+    data: z.object({
+        id: z.number(),
+        documentId: z.string(),
+        title: z.string(),
+        description: z.string().nullable(),
+        slug: z.string().nullable(),
+        publishedAt: z.string(),
+        updatedAt: z.string(),
+    }).nullable()
+})
+
 export async function GET(request: NextRequest) {
 
-    const blogsResponse = await fetch(`${process.env.STRAPI_URL}/articles`, {
+    const params = request.nextUrl.searchParams
+
+    const blogId = params.get('documentId') ? params.get('documentId') : ""
+
+    console.log(`Received request with params: ${params.toString()}`)
+
+    const blogServerResponse = await fetch(`${process.env.STRAPI_URL}/articles/${blogId}`, {
         headers: {
             'Authorization': `Bearer ${process.env.STRAPI_API_KEY}`,
-        }
+        },
     })
 
-    const blogs = await blogsResponse.json()    
+    const blogData = await blogServerResponse.json()
 
-    console.log(`API Response : ${JSON.stringify(blogs)}`)
+    console.log(`API Response : ${JSON.stringify(blogData)}`)
 
-    const response = BlogServerApiResponseSchema.parse(blogs)
+    const response = blogId ? BlogServerApiBlogResponseSchema.parse(blogData) : BlogServerApiBlogsResponseSchema.parse(blogData) 
 
     return Response.json({ response })
 
