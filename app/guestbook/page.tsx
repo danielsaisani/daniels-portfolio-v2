7a7962add1c312ee8d762ce83f54381dd2dd1ab6
DKS <47062269+DxnielKS@users.noreply.github.com>
2025-04-17 09:27:10 +0100

    Add guestbook disclaimer

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -3,6 +3,9 @@ import { getGuestbookEntries } from 'app/db/queries';
 import { SignIn, SignOut } from './buttons';
 import { Suspense } from 'react';
 import Form from './form';
+import { TriangleAlert } from 'lucide-react';
+
+const DISABLE_ADD_GUESTBOOOK_FORM = true
 
 export const metadata = {
   title: 'Guestbook',
@@ -13,11 +16,13 @@ export default function GuestbookPage() {
   return (
     <section className={'animate-fadeIn'}>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        leave your mark
+        leave your feedback, thoughts, or just say hi!
       </h1>
       <Suspense>
-        <GuestbookForm />
-        <GuestbookEntries />
+        <div className='space-y-10'>
+          <GuestbookForm />
+          <GuestbookEntries />
+        </div>
       </Suspense>
     </section>
   );
@@ -28,11 +33,15 @@ async function GuestbookForm() {
 
   return session?.user ? (
     <>
-      <Form />
-      <SignOut />
+      {DISABLE_ADD_GUESTBOOOK_FORM && <div className='rounded-md bg-tertiary p-2 flex justify-center items-center'><span>Guestbook Temporarily Disabled</span></div>}
+      {!DISABLE_ADD_GUESTBOOOK_FORM && <Form />}
+      {!DISABLE_ADD_GUESTBOOOK_FORM && <SignOut />}
     </>
   ) : (
-    <SignIn />
+    <>
+      {DISABLE_ADD_GUESTBOOOK_FORM && <div className='rounded-md bg-tertiary p-2 flex justify-center items-center space-x-2'><TriangleAlert/><span className='text-white'>Guestbook Temporarily Disabled</span></div>}
+      {!DISABLE_ADD_GUESTBOOOK_FORM && <SignIn />}
+    </>
   );
 }
 
