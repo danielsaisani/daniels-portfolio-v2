fa2167db36555c1ffaa4ea0268de4c0e3b9e376d
DKS <47062269+DxnielKS@users.noreply.github.com>
2025-06-24 13:55:27 +0100

    chore: temporarily remove three.js inspired component

--- app/components/Model.tsx
+++ app/components/Model.tsx
@@ -23,13 +23,17 @@ function Box(props: ThreeElements['mesh']) {
 
 function Model() {
     return (
-        <Canvas>
-            <ambientLight intensity={Math.PI / 2} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-        </Canvas>
+        <>
+        </>
+        // COMMENTED OUT BECAUSE OF SOME SILLY ERROR I CANNOT BE BOTHERED TO FIX RIGHT NOW - DANIEL
+        // TODO: PUT THIS BACK IN WHEN APPROPRIATE
+        // <Canvas>
+        //     <ambientLight intensity={Math.PI / 2} />
+        //     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        //     <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        //     <Box position={[-1.2, 0, 0]} />
+        //     <Box position={[1.2, 0, 0]} />
+        // </Canvas>
     );
 }
 
