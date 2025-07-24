a9bcf401a2d97cb5fb3af5281f535988b148e257
DKS <47062269+DxnielKS@users.noreply.github.com>
2025-07-24 09:31:33 +0100

    fix: make bujo stuff come up after typing is done.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,10 +3,11 @@
 import React, { useState } from "react";
 import TypingText from "./ui/text";
 import BulletJournalText from "./ui/BulletJournalText";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Hero: React.FC = () => {
     const [isHeaderTypingComplete, setisHeaderTypingComplete] = useState(false);
+    const [isBioTypingComplete, setisBioTypingComplete] = useState(false);
 
     return (
 
@@ -26,27 +27,30 @@ const Hero: React.FC = () => {
                         text={"full-stack software engineer, ex-SWE @ Terra YC W21 and comp sci grad @ King's College London"}
                         typingSpeed={10}
                         caretClassName="bg-white align-middle ml-[3px] inline-block"
+                        onComplete={() => setisBioTypingComplete(true)}
                     />
                 )}
             </p>
-
-            <motion.div className="mt-8"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 1.5 }}
-            >
-                <BulletJournalText text="[ ] An open task" />
-                <BulletJournalText text="[X] A completed task" />
-                <BulletJournalText text="o An event">
-                    <BulletJournalText text="o you've been confused by the presence of this bullet point, but then you remember a disclaimer I left on a previous blog, and now.. you understand" />
-                </BulletJournalText>
-                <BulletJournalText text="- A standard note">
-                    <BulletJournalText text="- An indented note" >
-                        <BulletJournalText text="- Another indented note" />
-                    </BulletJournalText>
-                </BulletJournalText>
-                <BulletJournalText text="!- An important note" />
-            </motion.div>
+            {isBioTypingComplete && (
+                <AnimatePresence>
+                    <motion.div className="mt-8"
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <BulletJournalText text="[ ] An open task" />
+                        <BulletJournalText text="[X] A completed task" />
+                        <BulletJournalText text="o An event">
+                            <BulletJournalText text="o you've been confused by the presence of this bullet point, but then you remember a disclaimer I left on a previous blog, and now.. you understand" />
+                        </BulletJournalText>
+                        <BulletJournalText text="- A standard note">
+                            <BulletJournalText text="- An indented note" >
+                                <BulletJournalText text="- Another indented note" />
+                            </BulletJournalText>
+                        </BulletJournalText>
+                        <BulletJournalText text="!- An important note" />
+                    </motion.div>
+                </AnimatePresence>)}
         </div>
 
     );
